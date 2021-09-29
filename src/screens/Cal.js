dad78acfd12ae221c3ba88923b6238ea740b1d4d
shashika05 <shashikaraveenyasas@gmail.com>
2021-09-30 01:51:00 +0530

    Added missed divided into Ui

--- src/screens/Cal.js
+++ src/screens/Cal.js
@@ -7,12 +7,24 @@ import Background from "../components/Background";
 
 const Cal = () => {
   const [text, setText] = useState("");
+  // const [dotPress, enableDotPress] = useState(false);
   const onNumPress = (keyPress) => {
     setText(text + keyPress);
   };
   const onClearButtonPress = () => {
     //
     setText(text.slice(0, -1));
+    // enableDotPress(false);
+  };
+
+  const onEqualPress = () => {
+    // Equal Handle
+    console.log(typeof Number(text), Number(text));
+  };
+
+  const dotPressTextHanle = () => {
+    // enableDotPress(true);
+    text === "" ? setText(0 + ".") : setText(text + ".");
   };
   return (
     <View style={tailwind("flex items-center w-full h-full")}>
@@ -26,7 +38,9 @@ const Cal = () => {
       >
         <TextInput
           style={[
-            tailwind("w-72 h-12 border-b-2 border-gray-800 rounded-xl pr-14"),
+            tailwind(
+              "w-72 h-12 border-b-2 border-gray-800 mt-2 rounded-xl pr-14"
+            ),
             { fontSize: 24, fontFamily: "M400" },
           ]}
           value={text}
@@ -40,13 +54,14 @@ const Cal = () => {
           activeOpacity={0.6}
           style={[
             tailwind("justify-center items-center absolute"),
-            { right: 16, top: 5 },
+            { right: 16, top: 14 },
           ]}
           onPress={() => onClearButtonPress()}
+          onLongPress={() => setText("")}
         >
           <Ionicons name="ios-backspace" size={32} color="black" />
         </TouchableOpacity>
-        <View style={tailwind("flex flex-row w-72 h-16 mt-2 ml-6")}>
+        <View style={tailwind("flex flex-row w-72 h-16 mt-5 ml-6")}>
           <TouchableOpacity
             activeOpacity={0.8}
             style={tailwind(
@@ -79,8 +94,9 @@ const Cal = () => {
             style={tailwind(
               "justify-center items-center w-14 h-16 bg-blue-400 rounded-xl ml-2"
             )}
+            // Plus sign
           >
-            <Text style={{ fontFamily: "M600", fontSize: 24 }}>x</Text>
+            <Text style={{ fontFamily: "M600", fontSize: 24 }}>+</Text>
           </TouchableOpacity>
         </View>
         <View style={tailwind("flex flex-row w-72 h-16 mt-2 ml-6")}>
@@ -116,6 +132,7 @@ const Cal = () => {
             style={tailwind(
               "justify-center items-center w-14 h-16 bg-blue-400 rounded-xl ml-2"
             )}
+            // Minus sign
           >
             <Text style={{ fontFamily: "M600", fontSize: 24 }}>-</Text>
           </TouchableOpacity>
@@ -153,8 +170,9 @@ const Cal = () => {
             style={tailwind(
               "justify-center items-center w-14 h-16 bg-blue-400 rounded-xl ml-2"
             )}
+            // Multipy sign
           >
-            <Text style={{ fontFamily: "M600", fontSize: 24 }}>+</Text>
+            <Text style={{ fontFamily: "M600", fontSize: 24 }}>×</Text>
           </TouchableOpacity>
         </View>
         <View style={tailwind("flex flex-row w-72 h-16 mt-2 ml-6")}>
@@ -181,6 +199,7 @@ const Cal = () => {
             style={tailwind(
               "justify-center items-center w-14 h-16 bg-blue-400 rounded-xl ml-2"
             )}
+            onPress={() => dotPressTextHanle()}
           >
             <Text style={{ fontFamily: "M600", fontSize: 24 }}>.</Text>
           </TouchableOpacity>
@@ -189,10 +208,26 @@ const Cal = () => {
             style={tailwind(
               "justify-center items-center w-14 h-16 bg-blue-400 rounded-xl ml-2"
             )}
+            // Divide sign
           >
-            <Text style={{ fontFamily: "M600", fontSize: 24 }}>=</Text>
+            <Text style={{ fontFamily: "M600", fontSize: 24 }}>÷</Text>
           </TouchableOpacity>
         </View>
+        <TouchableOpacity
+          // Equal Sign
+          activeOpacity={0.8}
+          style={[
+            tailwind(
+              "absolute justify-center items-center w-16 h-16 bg-gray-800 rounded-full ml-2"
+            ),
+            { right: -40, bottom: -40 },
+          ]}
+          onPress={() => onEqualPress()}
+        >
+          <Text style={{ fontFamily: "M600", fontSize: 24, color: "white" }}>
+            =
+          </Text>
+        </TouchableOpacity>
       </View>
       {/* --- BackGroud --- */}
       <Background />
