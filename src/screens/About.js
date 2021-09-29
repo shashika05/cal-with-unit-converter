7325b3c63347f320acac6a24bbe98c840908efe5
shashika05 <shashikaraveenyasas@gmail.com>
2021-09-29 10:31:43 +0530

    Added basic styles

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
+import tailwind from "tailwind-rn";
 
 const About = () => {
   return (
-    <View>
+    <View style={tailwind("flex items-center w-full h-full")}>
       <Text>About</Text>
+      <View
+        style={tailwind(
+          "absolute w-72 h-72 -bottom-12 -right-12 bg-blue-200 rounded-full"
+        )}
+      />
+      <View
+        style={tailwind(
+          "absolute w-80 h-96 -bottom-12 -left-12 bg-red-200 rounded-full"
+        )}
+      />
     </View>
   );
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import tailwind from "tailwind-rn";
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={tailwind("flex items-center w-full h-full")}>
+      <TouchableOpacity
+        style={tailwind(
+          "w-72 h-20 bg-gray-800 rounded-xl items-center justify-center mt-12"
+        )}
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate("Cal")}
+      >
+        <Text style={{ fontFamily: "M500", fontSize: 24, color: "white" }}>
+          Calculator
+        </Text>
+      </TouchableOpacity>
+      {/* --- Absolute View for BG --- */}
+      <View
+        style={tailwind(
+          "absolute w-72 h-72 -bottom-12 -right-12 bg-blue-200 rounded-full"
+        )}
+      />
+      <View
+        style={tailwind(
+          "absolute w-80 h-96 -bottom-12 -left-12 bg-red-200 rounded-full"
+        )}
+      />
     </View>
   );
 };
