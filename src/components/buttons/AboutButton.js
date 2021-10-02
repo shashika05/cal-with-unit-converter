ea599d5ff54990ee6aeeb96a0826b997a86beb32
shashika05 <shashikaraveenyasas@gmail.com>
2021-10-02 20:20:23 +0530

    Added Alert in Calculator Screen

--- src/components/buttons/AboutButton.js
+++ src/components/buttons/AboutButton.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
-const AboutButton = () => {
-  const nagigation = useNavigation();
+const AboutButton = ({ calAlert }) => {
+  const navigation = useNavigation();
+
+  const alertHandle = () => {
+    Alert.alert("Calculator", "version 1.0.0", [
+      {
+        text: "OK",
+        style: "cancel",
+      },
+      { text: "More", onPress: () => navigation.navigate("About") },
+    ]);
+  };
   return (
     <TouchableOpacity
       activeOpacity={0.8}
-      onPress={() => nagigation.navigate("About")}
+      onPress={() =>
+        calAlert === true ? alertHandle() : navigation.navigate("About")
+      }
     >
       <Feather name="info" size={24} color="black" />
     </TouchableOpacity>
