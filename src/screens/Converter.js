dde8504225655764958613cf17b7fc0b02f7b290
shashika05 <shashikaraveenyasas@gmail.com>
2021-10-09 03:28:33 +0530

    Finished handle Picker

--- src/screens/Converter.js
+++ src/screens/Converter.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
 import tailwind from "tailwind-rn";
+
+// Import Converter Array
+import converter from "../../ConverterArray";
 
 import Background from "../components/Background";
 
-const Converter = () => {
+const Converter = ({ conversion, setConversion }) => {
+  const converterArray = converter;
+  console.log(conversion);
   return (
     <View style={tailwind("flex items-center w-full h-full")}>
-      <Text>Converter</Text>
+      <Picker
+        style={tailwind("w-full h-12")}
+        selectedValue={conversion}
+        onValueChange={(itemValue, itemIndex) => setConversion(itemValue)}
+        itemStyle={{ fontFamily: "M600" }}
+      >
+        {converterArray.map((item, index) => {
+          return (
+            <Picker.Item label={item.label} value={item.value} key={index} />
+          );
+        })}
+      </Picker>
       {/* --- BackGroud --- */}
       <Background />
     </View>
