2840470111c879dbc1562a0cac1260f7de932e20
shashika05 <shashikaraveenyasas@gmail.com>
2021-10-09 03:27:40 +0530

    Created converters Array

---
+++ ConverterArray.js
@@ -0,0 +1,74 @@
+const converters = [
+  {
+    id: 1,
+    label: "Area",
+    value: "area",
+  },
+  {
+    id: 2,
+    label: "Data Transfer Rate",
+    value: "dataTransferRate",
+  },
+  {
+    id: 3,
+    label: "Digital Storage",
+    value: "digitalStorage",
+  },
+  {
+    id: 4,
+    label: "Energy",
+    value: "energy",
+  },
+  {
+    id: 5,
+    label: "Frequency",
+    value: "frequency",
+  },
+  {
+    id: 6,
+    label: "Fuel Economy",
+    value: "fuelEconomy",
+  },
+  {
+    id: 7,
+    label: "Length",
+    value: "length",
+  },
+  {
+    id: 8,
+    label: "Mass",
+    value: "mass",
+  },
+  {
+    id: 9,
+    label: "Plane Angle",
+    value: "planeAngle",
+  },
+  {
+    id: 10,
+    label: "Pressure",
+    value: "pressure",
+  },
+  {
+    id: 11,
+    label: "Speed",
+    value: "speed",
+  },
+  {
+    id: 12,
+    label: "Temperature",
+    value: "temperature",
+  },
+  {
+    id: 13,
+    label: "Time",
+    value: "time",
+  },
+  {
+    id: 14,
+    label: "Volume",
+    value: "volume",
+  },
+];
+
+export default converters;
